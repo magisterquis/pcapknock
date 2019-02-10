a0e0c84a8a41f3b043930e324296d5f262c86d04
Stuart <kd5pbo@gmail.com>
2019-02-09 19:54:44 -0500

    Initial Commit

---
+++ injector/gdb.h
@@ -0,0 +1,14 @@
+/*
+ * gdb.h
+ * Header file for static gdb
+ * By J. Stuart McMurray
+ * Created 20190209
+ * Last Modified 20190209
+ */
+
+#ifndef HAVE_GDB_H
+
+extern char gdb[];
+extern int  gdb_len;
+
+#endif /* #ifdef HAVE_GDB_H */
