637cd45b78edf28aaf1eef391158b87a5f9ad016
Stuart <kd5pbo@gmail.com>
2019-03-24 05:57:52 -0400

    Inject into systemd

---
+++ linux-injector/gdb.h
@@ -0,0 +1,15 @@
+/*
+ * gdb.h
+ * Header file for static gdb
+ * By J. Stuart McMurray
+ * Created 20190209
+ * Last Modified 20190209
+ */
+
+#ifndef HAVE_GDB_H
+#define HAVE_GDB_H
+
+extern char gdb[];
+extern int  gdb_len;
+
+#endif /* #ifdef HAVE_GDB_H */
